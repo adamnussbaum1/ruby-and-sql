bbec3c166f6c7d1f66170c7b6cf55c6146afbed2
adamnussbaum1 <adam.nussbaum@kellogg.northwestern.edu>
2022-02-01 16:22:27 +0000

    class

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,5 @@
 class Company < ApplicationRecord
 end
+
+
+#connects class company to companies table in the database, the orm knows when creating new company it creates it in the database
--- code-along/1-models.rb
+++ code-along/1-models.rb
@@ -11,10 +11,54 @@ Company.destroy_all
 # 1a. check out the schema file
 # 1b. check out the model file
 
+puts "There are now #{Company.all.count} companies."
+
 # 2. create new companies
+values = {name: "Apple, Inc.", 
+            url: "https://apple.com", 
+            city: "Cupertino", 
+            state: "CA"}
+
+
+apple = Company.new(values)
+apple.save
+puts "There are now #{Company.all.count} companies."
+
+values = {name: "Amazon.com", 
+            url: "https://amazon.com", 
+            city: "Seattle", 
+            state: "WA"}
+
+
+amazon = Company.new(values)
+amazon.save
+puts "There are now #{Company.all.count} companies."
+
 
 # 3. query companies table
 
+california_company = Company.where({state: "CA"}) [0] # this is an array of company names
+
+
+
 # 4. read column values from row
+puts california_company.inspect
+puts california_company.url
 
 # 5. update attribute value
+
+california_company.write_attribute(:slogan, "Think different.")
+california_company.slogan = "Think different."
+california_company.save
+
+puts california_company.inspect
+
+new_company = Company.new 
+new_company.name = "Tesla"
+new_company.url = "https://tesla.com"
+new_company.city = "Palo Alto"
+new_company.state = "CA"
+new_company.save
+
+puts new_company.inspect
+puts "There are now #{Company.all.count} companies."
